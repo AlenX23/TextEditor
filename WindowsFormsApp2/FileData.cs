0513bf5dd813a3ed34dd6bba6ea17b12a9cb9d34
AlenX23 <alenx6123@gmail.com>
2020-06-11 10:02:36 +0300

    1

---
+++ WindowsFormsApp2/FileData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    class FileData
+    {
+        public FileInfo file;
+        public string Content;
+        public bool IsNotSaveOnce = true;
+
+        public event EventHandler<string> DidUpdateContent;
+        public FileData()
+        {
+            file = new FileInfo("NewFile");
+            Content = "";
+        }
+
+        public FileData(string filename)
+        {
+            file = new FileInfo(filename);
+            Content = file.OpenText().ReadToEnd();
+        }
+    }
+}
